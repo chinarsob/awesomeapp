d7cbd63d252a3bc9b095c1f82b05dcd1b7ec2b3e
Chinar <chinar@Varuns-MacBook-Air.local>
2022-01-24 13:54:53 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Javascript knowledge is must before learning React.
         </p>
         <a
           className="App-link"
@@ -15,9 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello world
         </a>
-      </header>
+        </p>
     </div>
   );
 }
